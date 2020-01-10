e647d16e7a1666f388c7be28feb10bf5a6b04bfb
Carmen Montiel <52963336+kimmonti@users.noreply.github.com>
2020-01-10 11:16:25 -0300

    Update missionthree.py

--- missionthree.py
+++ missionthree.py
@@ -1,19 +1,19 @@
 print("Este programa imprime los números de un rango cualquiera donde, para los múltiplos de tres imprime Fizz en lugar del número y para los múltiplos de cinco imprime Buzz. Para los números que son múltiplos de tres y cinco, escribe FizzBuzz")
 
-def fizzbuzzgame(minimum,maximum):
-  list_number = []
-  for x in range(minimum,maximum):
-    if x % 3 == 0:
-        list_number.append("Fizz")
-    elif x % 5 == 0:
-        list_number.append("Buzz")
-    elif x % 3 == 0 and x % 5 == 0:
-        list_number.append("FizzBuzz")
+def fizzbuzzgame(minimum,maximum): #Definimos la variable del juego
+  list_number = [] #Creamos una lista vacía
+  for x in range(minimum,maximum): #Iniciamos un bucle for con rangos a introducir
+    if x % 3 == 0: #Verificamos si el número del rango es múltiplo de 3
+        list_number.append("Fizz") #Agregamos Fizz en el lugar del número
+    elif x % 5 == 0: #Verificamos si el número del rango es múltiplo de 5
+        list_number.append("Buzz") #Agregamos Buzz en el lugar del número
+    elif x % 3 == 0 and x % 5 == 0: #Verificamos si el número del rango es múltiplo de 3 y 5
+        list_number.append("FizzBuzz") #Agregamos FizzBuzz en el lugar del número
     else:
-        list_number.append(x)
-  print(list_number)
+        list_number.append(x) #Agregamos a la lista el resto de los números
+  print(list_number) #Imprimimos la lista vacía
 
-first= int(input("Inserte el valor mínimo del rango:"))
-last= int(input("Inserte el valor máximo del rango:"))
-fizzbuzzgame(first,last)
+first= int(input("Inserte el valor mínimo del rango:")) #Introducimos el valor mínimo del rango
+last= int(input("Inserte el valor máximo del rango:")) #Introducimos el valor máximo del rango
+fizzbuzzgame(first,last) #Llamamos a la función
 
