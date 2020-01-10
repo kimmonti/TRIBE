4177cf7ebc92264685cd7fb6e2f825b92778dcbf
Carmen Montiel <52963336+kimmonti@users.noreply.github.com>
2020-01-10 12:47:59 -0300

    Update missionthree.py

--- missionthree.py
+++ missionthree.py
@@ -3,12 +3,12 @@ print("Este programa imprime los números de un rango cualquiera donde, para los
 def fizzbuzzgame(minimum,maximum): #Definimos la variable del juego
   list_number = [] #Creamos una lista vacía
   for x in range(minimum,maximum): #Iniciamos un bucle for con rangos a introducir
-    if x % 3 == 0: #Verificamos si el número del rango es múltiplo de 3
-        list_number.append("Fizz") #Agregamos Fizz en el lugar del número
+    if x % 3 == 0 and x % 5 == 0: #Verificamos si el número del rango es múltiplo de 3 y 5
+        list_number.append("FizzBuzz") #Agregamos FizzBuzz en el lugar del número
     elif x % 5 == 0: #Verificamos si el número del rango es múltiplo de 5
         list_number.append("Buzz") #Agregamos Buzz en el lugar del número
-    elif x % 3 == 0 and x % 5 == 0: #Verificamos si el número del rango es múltiplo de 3 y 5
-        list_number.append("FizzBuzz") #Agregamos FizzBuzz en el lugar del número
+    elif x % 3 == 0: #Verificamos si el número del rango es múltiplo de 3
+        list_number.append("Fizz") #Agregamos Fizz en el lugar del número
     else:
         list_number.append(x) #Agregamos a la lista el resto de los números
   print(list_number) #Imprimimos la lista vacía
